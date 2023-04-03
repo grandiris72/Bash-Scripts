6b9a0a6b4375449d1f9f305994d3ca531972efbd
Patrick Ross <85891828+grandiris72@users.noreply.github.com>
2023-04-03 13:45:18 -0500

    create ansibleinstall.sh
    installs ansible as well as dependencies for aws ec2 creation

---
+++ ansibleinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add Ansible PPA repository
+sudo add-apt-repository -y ppa:ansible/ansible
+
+# Update package lists
+sudo apt-get update
+
+# Install necessary packages
+sudo apt-get install -y software-properties-common python3-pip
+sudo pip3 install botocore boto3 ansible
+
+# Install Ansible collections
+ansible-galaxy collection install community.aws
+ansible-galaxy collection install amazon.aws
+ansible-galaxy collection install newrelic.newrelic-infra
