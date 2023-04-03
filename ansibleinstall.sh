97c95c830f8272efc1dabf5418db1927d8ab2f45
Patrick Ross <85891828+grandiris72@users.noreply.github.com>
2023-04-03 14:17:57 -0500

    added wgets for yml and cfg
    added wgets for playbook files, replace default ansible.cfg with preconfigured.

--- ansibleinstall.sh
+++ ansibleinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add Ansible PPA repository
-sudo add-apt-repository -y ppa:ansible/ansible
+add-apt-repository -y ppa:ansible/ansible
 
 # Update package lists
 sudo apt-get update
@@ -14,3 +14,12 @@ pip3 install botocore boto3 ansible
 ansible-galaxy collection install community.aws
 ansible-galaxy collection install amazon.aws
 ansible-galaxy collection install newrelic.newrelic-infra
+
+wget https://raw.githubusercontent.com/grandiris72/ansible-playbooks/main/createawsec2instance.yml
+wget https://raw.githubusercontent.com/grandiris72/ansible-playbooks/main/aws_ec2.yaml
+
+echo replacing ansible config
+cd /etc/ansible
+sudo rm -rf ansible.cfg
+wget https://raw.githubusercontent.com/grandiris72/ansible-playbooks/main/ansible.cfg
+cd
